7dba1c17d24c0580a76be3b12d08c7332f7f233e
nohosh <sanchit8b@gmail.com>
2019-11-03 19:44:33 +0530

    apache derby crudRepo

--- src/main/java/io/javabrains/springbootquickstart/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootquickstart/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootquickstart.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,6 +10,9 @@ import java.util.List;
 @Service
 public class TopicService {
 
+    @Autowired
+    private TopicRepository topicRepository;
+
     private List<Topic> topics = new ArrayList< >(Arrays.asList(
             new Topic("spring","spring Framework","spring framework description"),
             new Topic("java","java Framework","java framework description"),
@@ -16,6 +20,9 @@ public class TopicService {
     ));
 
     public List<Topic> getAllTopics() {
+        // return topics;
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
     public Topic getTopic (String id) {
@@ -23,7 +30,8 @@ public class TopicService {
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        //topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(Topic topic, String id) {
