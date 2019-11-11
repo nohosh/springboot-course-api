a5e9bd13312e1620c2c72c9bb8f18c9a421b91a9
nohosh <sanchit8b@gmail.com>
2019-11-11 23:30:17 +0530

    Static list removed

--- src/main/java/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/javabrains/springbootstarter/topic/TopicService.java
@@ -13,12 +13,6 @@ public class TopicService {
     @Autowired
     private TopicRepository topicRepository;
 
-    private List<Topic> topics = new ArrayList< >(Arrays.asList(
-            new Topic("spring","spring Framework","spring framework description"),
-            new Topic("java","java Framework","java framework description"),
-            new Topic("js","js Framework","js framework description")
-    ));
-
     public List<Topic> getAllTopics() {
         // return topics;
         List<Topic> topics = new ArrayList<>();
@@ -26,7 +20,7 @@ public class TopicService {
         return topics;
     }
     public Topic getTopic (String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findOne(id);
     }
 
     public void addTopic(Topic topic) {
@@ -35,16 +29,10 @@ public class TopicService {
     }
 
     public void updateTopic(Topic topic, String id) {
-        for (int i = 0; i < topics.size(); i++){
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)){
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic( String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.delete(id);
     }
 }
